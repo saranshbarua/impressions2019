50325182556e7b763bea3b398a908de5347e67e8
saranshbarua <saranshbarua@gmail.com>
2018-12-21 00:26:57 +0530

    add logo

--- src/components/LandingPage/landingPage.js
+++ src/components/LandingPage/landingPage.js
@@ -4,6 +4,7 @@ const landingPage = () => {
   return (
     <div className="lp-container">
         <div className="lp-main pf-400">
+            <h2 className="lato-300 logo">IMPRESSIONS</h2>
             <span className="wow appear">
               Feel the wanderlust
             </span>
