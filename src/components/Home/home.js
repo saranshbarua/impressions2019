4a66089875c69d6255d09cbb9e49e91f5d22b5e9
saranshbarua <saranshbarua@gmail.com>
2018-12-21 00:12:23 +0530

    try wow and add react-reveal

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import WOW from 'wow.js'
 
 //Sections
 
 import LandingPage from '../LandingPage/landingPage'
 
 export default class Home extends Component {
+  componentDidMount() {
+    const wow = new WOW();
+    wow.init();
+  }
+
   render() {
     return (
       <div className="main">
--- src/components/LandingPage/landingPage.js
+++ src/components/LandingPage/landingPage.js
@@ -4,7 +4,9 @@ const landingPage = () => {
   return (
     <div className="lp-container">
         <div className="lp-main pf-400">
-            Feel the wanderlust
+            <span className="wow appear">
+              Feel the wanderlust
+            </span>
         </div>
     </div>
   )
