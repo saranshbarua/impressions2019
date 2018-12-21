f16f1e8df1fc0dbd392413f10b67b077aaf8792c
saranshbarua <saranshbarua@gmail.com>
2018-12-22 01:24:04 +0530

    customize carousel

--- src/components/Carousel/carousel.js
+++ src/components/Carousel/carousel.js
@@ -4,32 +4,76 @@ import Slider from "react-slick";
 export default class ArtistCarousel extends Component {
   render() {
     const settings = {
-      dots: false,
+      className: "center",
+      centerMode: true,
       infinite: true,
+      centerPadding: "60px",
+      slidesToShow: 3,
       speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
     };
     return (
       <div className="caro">
         <Slider {...settings}>
-          <div style={{border: '1px solid red'}}>
+          <div>
+            <div className="artist-card">
             
+            </div>
           </div>
           <div>
-            <h3>2</h3>
+            <div className="artist-card">
+            
+            </div>
           </div>
           <div>
-            <h3>3</h3>
+            <div className="artist-card">
+            
+            </div>
           </div>
           <div>
-            <h3>4</h3>
+            <div className="artist-card">
+            
+            </div>
           </div>
           <div>
-            <h3>5</h3>
+            <div className="artist-card">
+            
+            </div>
           </div>
           <div>
-            <h3>6</h3>
+            <div className="artist-card">
+            
+            </div>
+          </div>
+          <div>
+            <div className="artist-card">
+            
+            </div>
           </div>
         </Slider>
       </div>
