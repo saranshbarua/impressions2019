38d74deb41a57853d2c87d4fa68ddc8c567000d0
saranshbarua <saranshbarua@gmail.com>
2018-12-24 23:39:12 +0530

    add new route for events

---
+++ src/components/Events/events.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Events = () => {
+  return (
+    <div>
+      this is the events page
+    </div>
+  )
+}
+
+export default Events
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom'
 
 //Components
 import Home from './components/Home/home'
+import Events from './components/Events/events'
 
 const Routes = () => {
     return (
         <Switch>
+            <Route path="/events" exact component={Events} />
             <Route path="/" exact component={Home} />
         </Switch>
     )
