6e27a22fe64dcdb87cac383d6cd672e81d51af89
saranshbarua <saranshbarua@gmail.com>
2018-12-25 20:41:18 +0530

    add hubs

--- src/components/Events/events.js
+++ src/components/Events/events.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Fade from 'react-reveal'
 
 const Events = () => {
   return (
     <div className="events-page">
-      <span className="pf-400 ev-head">Events</span>
+      <Fade top cascade><span className="pf-400 ev-head">Events</span></Fade>
+      <div className="hubs-cont">
+        <div className="hub-card">
+          <div className="hub-img">
+            <img src="https://media.licdn.com/dms/image/C4E0BAQExLsnLuNFZ8w/company-logo_200_200/0?e=2159024400&v=beta&t=_WDOcKLT02xVQmDxJ_F2r9rO0YMzYpfx0lBSA7sL6Tg"
+             alt="logo"
+             height="90%"
+             />
+          </div>
+          <div className="hub-content">
+              <span className="pf-400 hub-name">Knuth Programming Hub</span>
+              <span className="lato-400 small-lato event-count"> 2 EVENTS</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
