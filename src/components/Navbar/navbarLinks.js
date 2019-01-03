d364b43d820167fe17912c18e393ec510bb93836
saranshbarua <saranshbarua@gmail.com>
2019-01-03 10:30:01 +0530

    add team in navbar

--- src/components/Navbar/navbarLinks.js
+++ src/components/Navbar/navbarLinks.js
@@ -10,7 +10,7 @@ const NavbarLinks = (props) => {
           <li><AnchorLink className="pf-400 nav-links" href="#artistsSection" onClick={props.closeTheNav}>Artists</AnchorLink></li>
           <li><AnchorLink className="pf-400 nav-links" href="#eventsSection" onClick={props.closeTheNav}>Events</AnchorLink></li>
           <li><AnchorLink className="pf-400 nav-links" href="#sponsorsSection" onClick={props.closeTheNav}>Sponsors</AnchorLink></li>
-          {/* <li><AnchorLink href="#aboutSection" onClick={props.closeTheNav}>About Impressions</AnchorLink></li> */}
+          <li><AnchorLink className="pf-400 nav-links" href="#teamSection" onClick={props.closeTheNav}>Team</AnchorLink></li>
       </ul>
     </div>
   )
