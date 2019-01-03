1aa974439e7ca2f559beb8eb80d7cdc59dac2ad8
saranshbarua <saranshbarua@gmail.com>
2019-01-03 23:26:30 +0530

    add map

--- src/components/ContactUs/contactUs.js
+++ src/components/ContactUs/contactUs.js
@@ -13,7 +13,11 @@ const ContactUs = () => {
       <span className="lato-400 small-lato"> <span className="pf-400" style={{fontSize: '15px'}}>06</span> CONTACT US</span>
       <Fade duration={700} top cascade><span className="pf-400 section-heading">Reach Out</span></Fade>
       <Slide><p className="pf-400 venue">Jaypee Institute of Information Technology</p></Slide>
-      <div className="map"></div>
+      <div className="map">
+        <a href="https://www.google.com/maps/place/JAYPEE+INSTITUTE+OF+INFORMATION+TECHNOLOGY/@28.629431,77.3726531,17z/data=!4m19!1m13!4m12!1m4!2m2!1d77.3711296!2d28.6308149!4e1!1m6!1m2!1s0x390ce551491b3ce7:0x7335d9fcfd4d9db0!2sjaypee+institute+of+information+technology!2m2!1d77.3720556!2d28.6297291!3m4!1s0x390ce551491b3ce7:0x7335d9fcfd4d9db0!8m2!3d28.6297291!4d77.3720556">
+          <img src='./map.png' width="100%" alt="JIIT location"/>        
+        </a>
+      </div>
       <div className="lp-link-box cp-box">
             <div className="lp-link-boxes lp-link-box-one cp-boxes">
               <span className="lato-400 small-lato">EMAIL</span>
