840d8f5886e7fccf710b16d36265b977acb543f0
saranshbarua <saranshbarua@gmail.com>
2019-01-03 10:28:01 +0530

    add subhead

--- src/components/Sponsors/sponsors.js
+++ src/components/Sponsors/sponsors.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Fade from 'react-reveal/Fade'
+import Slide from 'react-reveal/Slide'
 
 const Sponsors = () => {
   return (
     <div className="sponsors-container" id="sponsorsSection">
        <span className="lato-400 small-lato"> <span className="pf-400" style={{fontSize: '15px'}}>04</span> SPONSORS</span>
         <Fade duration={700} top cascade><span className="pf-400 section-heading">Our Partners</span></Fade>
+        <Slide duration={700} bottom><span className="pf-400 section-heading section-subheading">Media Partners</span></Slide>
+        
     </div>
   )
 }
