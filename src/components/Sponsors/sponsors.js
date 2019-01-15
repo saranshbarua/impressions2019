3e3843684e90ae482952afa717a0f32aeba9d71f
saranshbarua <saranshbarua@gmail.com>
2019-01-15 23:33:07 +0530

    change navbar for big screens

--- src/components/Sponsors/sponsors.js
+++ src/components/Sponsors/sponsors.js
@@ -37,36 +37,7 @@ const Sponsors = () => {
        <span className="lato-400 small-lato"> <span className="pf-400" style={{fontSize: '15px'}}>04</span> SPONSORS</span>
         <Fade duration={700} top cascade><span className="pf-400 section-heading">Our Partners</span></Fade>
         <Slide duration={700} bottom><span className="pf-400 section-heading section-subheading">Media Partners</span></Slide>
-        {/* <div className="media-partner"> */}
-          <MapSponsors />
-          {/* <div className="spon-img">
-            <img src='./mediapartners/spon1.JPG' height="100%" alt=""/>
-          </div>
-
-          <div className="spon-img">
-            <img src='./mediapartners/spon1.JPG' height="100%" alt=""/>
-          </div>
-
-          <div className="spon-img">
-            <img src='./mediapartners/spon1.JPG' height="100%" alt=""/>
-          </div>
-
-          <div className="spon-img">
-            <img src='./mediapartners/spon1.JPG' height="100%" alt=""/>
-          </div>
-
-          <div className="spon-img">
-            <img src='./mediapartners/spon1.JPG' height="100%" alt=""/>
-          </div>
-
-          <div className="spon-img">
-            <img src='./mediapartners/spon1.JPG' height="100%" alt=""/>
-          </div>
-
-          <div className="spon-img">
-            <img src='./mediapartners/spon1.JPG' height="100%" alt=""/>
-          </div> */}
-        {/* </div> */}
+        <MapSponsors />
     </div>
   )
 }
