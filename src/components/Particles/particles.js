895fcd4a28212944b0b9dfa66466ff6ff7ed6655
saranshbarua <saranshbarua@gmail.com>
2019-01-19 20:36:33 +0530

    remove props from particles

--- src/components/Particles/particles.js
+++ src/components/Particles/particles.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Particles from 'react-particles-js'
 
-export default function ParticlesDiv(props) {
+export default function ParticlesDiv() {
   return (
     <React.Fragment>
       <Particles 
