a9c0357981739a2c577e3620698aed391eddcbdb
saranshbarua <saranshbarua@gmail.com>
2019-01-20 15:13:09 +0530

    add team images, and make basic layout of team page

--- src/components/Team/teamPage.js
+++ src/components/Team/teamPage.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Fade from 'react-reveal'
+import Slide from 'react-reveal/Slide'
 
 export default function TeamPage() {
   return (
-    <div>
-      Team Page
+    <div className="events-page">
+        <Fade top cascade><span className="pf-400 ev-head" style={{fontSize: '55px'}}>Team</span></Fade>
+        <div className="team-sub">
+          <Slide bottom><span className="pf-400 ts-head">Finance</span></Slide>
+          <div className="team-card">
+            <img src="./jycfinanceteam/apoorv.jpg" height="100%" width="100%" alt=""/>
+            
+          </div>
+        </div>
     </div>
   )
 }
