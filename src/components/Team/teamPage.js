edafdb1045d59f12c7608f69b2dd3f2749fd6068
saranshbarua <saranshbarua@gmail.com>
2019-01-20 15:42:13 +0530

    finalize card layout

--- src/components/Team/teamPage.js
+++ src/components/Team/teamPage.js
@@ -8,9 +8,16 @@ export default function TeamPage() {
         <Fade top cascade><span className="pf-400 ev-head" style={{fontSize: '55px'}}>Team</span></Fade>
         <div className="team-sub">
           <Slide bottom><span className="pf-400 ts-head">Finance</span></Slide>
+
           <div className="team-card">
             <img src="./jycfinanceteam/apoorv.jpg" height="100%" width="100%" alt=""/>
-            
+            <div className="mem-details">
+              <span className="mem-name pf-400">Apoorv Singhal</span>
+              <span className="mem-desig pf-400">Director</span>
+              <div className="mem-contact">
+                
+              </div>
+            </div>
           </div>
         </div>
     </div>
