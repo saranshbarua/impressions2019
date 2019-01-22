e0718ee97b27be6baeb37642cfc0101cc5150210
saranshbarua <saranshbarua@gmail.com>
2019-01-22 17:29:21 +0530

    add facebook and instagram links in navbar

--- src/components/Navbar/navbarLinks.js
+++ src/components/Navbar/navbarLinks.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { faInstagram, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+library.add(faInstagram, faFacebook, faTwitter);
+
 
 const NavbarLinks = (props) => {
   
@@ -12,6 +18,40 @@ const NavbarLinks = (props) => {
           <li><AnchorLink className="pf-400 nav-links" href="#sponsorsSection" onClick={props.closeTheNav}>Sponsors</AnchorLink></li>
           <li><AnchorLink className="pf-400 nav-links" href="#teamSection" onClick={props.closeTheNav}>Team</AnchorLink></li>
           <li><AnchorLink className="pf-400 nav-links" href="#contactSection" onClick={props.closeTheNav}>Contact Us</AnchorLink></li>
+          <br/>
+          <li>
+            <a
+              style={{
+                color: 'white'
+              }}
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.facebook.com/jiit.impressions/">
+              <FontAwesomeIcon 
+                icon={["fab", "facebook"]}
+                size="2x"
+                style={{
+                  margin: '0px 5px'
+                }}
+              />
+            </a> 
+            &nbsp;&nbsp;
+            <a
+              style={{
+                color: 'white'
+              }}
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.instagram.com/jiit.impressions/">
+              <FontAwesomeIcon 
+                icon={["fab", "instagram"]}
+                size="2x"
+                style={{
+                  margin: '0px 5px'
+                }}
+              />
+            </a> 
+          </li>
       </ul>
     </div>
   )
