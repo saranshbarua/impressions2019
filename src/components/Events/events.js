96b1d303317ec6007d7bbc18cbbef857bed49549
saranshbarua <saranshbarua@gmail.com>
2019-01-31 20:26:02 +0530

    map hubs

--- src/components/Events/events.js
+++ src/components/Events/events.js
@@ -2,25 +2,119 @@ import React from 'react'
 import Fade from 'react-reveal'
 import { Link } from 'react-router-dom'
 
+const hubInfo = [
+  {
+    name: 'Icreate',
+    eventCount: '2',
+    link: '/icreate'
+  },
+  {
+    name: 'Jhankar',
+    eventCount: '4',
+    link: '/jhankar'
+  },
+  {
+    name: 'JPEG',
+    eventCount: '3',
+    link: '/jpeg'
+  },
+  {
+    name: 'CICE',
+    eventCount: '3',
+    link: '/cice'
+  },
+  {
+    name: 'Graphicas',
+    eventCount: '1',
+    link: '/graphicas'
+  },
+  {
+    name: 'Cresendo',
+    eventCount: '4',
+    link: '/cresendo'
+  },
+  {
+    name: 'IOE',
+    eventCount: '2',
+    link: 'ioe'
+  },
+  {
+    name: 'Kalakriti',
+    eventCount: '2',
+    link: '/kalakriti'
+  },
+  {
+    name: 'DSC',
+    eventCount: '2',
+    link: '/dsc'
+  },
+  {
+    name: 'Knuth',
+    eventCount: '3',
+    link: '/knuth'
+  },
+  {
+    name: 'Parola',
+    eventCount: '2',
+    link: '/parola'
+  },
+  {
+    name: 'UCR',
+    eventCount: '4',
+    link: '/ucr'
+  },
+  {
+    name: 'Game development hub',
+    eventCount: '3',
+    link: '/gdh'
+  },
+  {
+    name: 'Radiance',
+    eventCount: '3',
+    link: '/radiance'
+  },
+  {
+    name: 'Page turner society',
+    eventCount: '2',
+    link: '/pts'
+  },
+  {
+    name: 'Thespian',
+    eventCount: '3',
+    link: '/thespian'
+  },
+  {
+    name: 'Adwitiya',
+    eventCount: '2',
+    link: '/adwitiya'
+  }
+];
+
 const Events = () => {
   return (
     <div className="events-page">
       <Fade top cascade><span className="pf-400 ev-head">Events</span></Fade>
       <div className="hubs-cont">
-        <Link style={{outline: 'none', textDecoration: 'none'}} to={`/eventlol/knuth`}>
-          <div className="hub-card">
-            <div className="hub-img">
-              <img src="https://media.licdn.com/dms/image/C4E0BAQExLsnLuNFZ8w/company-logo_200_200/0?e=2159024400&v=beta&t=_WDOcKLT02xVQmDxJ_F2r9rO0YMzYpfx0lBSA7sL6Tg"
-              alt="logo"
-              height="90%"
-              />
-            </div>
-            <div className="hub-content">
-                <span className="pf-400 hub-name">Knuth Programming Hub</span>
-                <span className="lato-400 small-lato event-count"> 2 EVENTS</span>
-            </div>
-          </div>
-        </Link>
+      {
+        hubInfo.map((item,i) => {
+          return (
+              <Link style={{outline: 'none', textDecoration: 'none', color: 'white'}} to={`/eventlol${item.link}`} key={i}>
+                <div className="hub-card">
+                  <div className="hub-img">
+                    <img src="https://media.licdn.com/dms/image/C4E0BAQExLsnLuNFZ8w/company-logo_200_200/0?e=2159024400&v=beta&t=_WDOcKLT02xVQmDxJ_F2r9rO0YMzYpfx0lBSA7sL6Tg"
+                    alt="logo"
+                    height="90%"
+                    />
+                  </div>
+                  <div className="hub-content">
+                      <span className="pf-400 hub-name">{item.name}</span>
+                      <span className="lato-400 small-lato event-count">{item.eventCount} Events</span>
+                  </div>
+                </div>
+              </Link>
+          )
+        })
+      }
       </div>
     </div>
   )
--- src/components/Events/hubEvents.js
+++ src/components/Events/hubEvents.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 import Slide from 'react-reveal/Slide'
 import eventsInfo from './eventStore'
 
-export default class hubEvents extends Component {
-
+export default class HubEvents extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -12,44 +11,42 @@ export default class hubEvents extends Component {
   }
 
   mapEvents() {
-    for(let i=1;i<eventsInfo[this.state.hub].events.length;i++) {
+    for (let i = 1; i < eventsInfo[this.state.hub].events.length; i++) {
       return (
         <div className="event-box ep-ex-box" key={i}>
-            <span className="ep-heading pf-400">
-              <Slide bottom>{eventsInfo[this.state.hub].events[i].name}</Slide>
-            </span>
-            <Slide>
-              <span className="lato-400 ep-desc">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+          <span className="ep-heading pf-400">
+            <Slide bottom>{eventsInfo[this.state.hub].events[i].name}</Slide>
+          </span>
+          <Slide>
+            <span className="lato-400 ep-desc">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
               </span>
-              <button className="ep-register pf-400">Register now</button>
-            </Slide>
-          </div>
+            <button className="ep-register pf-400">Register now</button>
+          </Slide>
+        </div>
       )
     }
   }
 
   render() {
-    console.log(eventsInfo[this.state.hub])
     return (
       <div className="ep-cont">
         <span className="pf-400 ep-hub-name"><Slide top cascade>{eventsInfo[this.state.hub].hubName}</Slide></span>
-
         <div className="ep-top">
           <div className="ep-logo">
-          <img width="100%" src="https://media.licdn.com/dms/image/C4E0BAQExLsnLuNFZ8w/company-logo_200_200/0?e=2159024400&v=beta&t=_WDOcKLT02xVQmDxJ_F2r9rO0YMzYpfx0lBSA7sL6Tg" alt="hub logo"/>
+            <img width="100%" src="https://media.licdn.com/dms/image/C4E0BAQExLsnLuNFZ8w/company-logo_200_200/0?e=2159024400&v=beta&t=_WDOcKLT02xVQmDxJ_F2r9rO0YMzYpfx0lBSA7sL6Tg" alt="hub logo" />
           </div>
           <div className="event-box">
             <span className="ep-heading pf-400">
-              <Slide bottom></Slide>
+              <Slide bottom>{eventsInfo[this.state.hub].events[0].name}</Slide>
             </span>
             <Slide>
               <span className="lato-400 ep-desc">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
               </span>
               <button className="ep-register pf-400">Register now</button>
             </Slide>
