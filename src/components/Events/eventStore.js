c340d5a7c1fde962f0b040ab5c48e284f7039d3f
saranshbarua <saranshbarua@gmail.com>
2019-01-31 10:23:27 +0530

    add events for testing

--- src/components/Events/eventStore.js
+++ src/components/Events/eventStore.js
@@ -1,8 +1,62 @@
 const eventsInfo = {
   knuth: [
     {
-      name: "KnuthCup",
-      time: "JNSJNS"
+      name: 'Imagine your Korea',
+      desc: '',
+      time: '',
+      room: ''
+    },
+    {
+      name: 'Test Your Bond',
+      desc: '',
+      time: '',
+      room: ''
+    }
+  ],
+  jhankar: [
+    {
+      name: 'Dance of Fame : Western',
+      desc: '',
+      time: '01:00 P.M - 5:00 P.M',
+      room: 'Audi- D1 + all F1-F10(JBS)(Whole Day)'
+    },
+    {
+      name: 'Dance of Fame : Classical',
+      desc: '',
+      time: '01:00 P.M- 05:00 P.M',
+      room: 'Audi- D1 + all F1-F10(JBS)(Whole Day)'
+    },
+    {
+      name: 'Fire groove - solo dance competetion',
+      desc: '',
+      time: '01:00 P.M- 05:00 P.M',
+      room: 'Audi - D2 + Green Rooms'
+    },
+    {
+      name: 'On the spot',
+      desc: '',
+      time: '01:00 P.M- 05:00 P.M',
+      room: 'Audi - D2 + Green Rooms'
+    }
+  ],
+  JPEG: [
+    {
+      name: 'Mr. and miss Photogenic',
+      desc: '',
+      time: 'Whole Day',
+      room: 'Photo booth - Annapurna - D1 D2'
+    },
+    {
+      name: 'Piksel',
+      desc: '',
+      time: 'Whole Day',
+      room: 'Annapurna Wall - D1 - D2( Decoration)'
+    },
+    {
+      name: 'Just do it',
+      desc: '',
+      time: '12:00 Noon - 03:00 P.M',
+      room: 'D2 - LT5'
     }
   ]
 }
